bc39fea69e9329a0d8bc25ab0617a7aac1735d0f
Mr.  Little Zhu <zhumavip@163.com>
2017-09-22 18:39:03 +0800

    Update 4.support_vector_machine.py

--- 4.support_vector_machine.py
+++ 4.support_vector_machine.py
@@ -12,13 +12,13 @@ import numpy
 import cvxopt
 import matplotlib.pyplot as plt
 
-def linearKernel(x1,x2):
+def linearKernel(x1,x2):        #the linear kernel function
     return numpy.dot(x1,x2)
 
-def gaussianKernel(x1,x2,sigma = 3):
+def gaussianKernel(x1,x2,sigma = 3):       #the guassian kernel function
     return numpy.exp(-numpy.linalg.norm(x1-x2)**2/(2*(sigma ** 2)))
 
-def makeKernelMatrix(input_,kernel,par = 3):
+def makeKernelMatrix(input_,kernel,par = 3):      #set up kernel matrix according to the kernel
     num = input_.shape[0]
     K = numpy.zeros((num,num))
     for i in range(num):
@@ -29,7 +29,7 @@ def makeKernelMatrix(input_,kernel,par = 3):
                 K[i,j] = gaussianKernel(input_[i],input_[j],par)
     return K
 
-def calculateA(input_,label,C,K):
+def calculateA(input_,label,C,K):          #using cvxopt bag figure out the convex quadratic programming
     num = input_.shape[0]
     P = cvxopt.matrix(numpy.outer(label,label)*K)
     q = cvxopt.matrix(numpy.ones(num)*-1)
@@ -45,59 +45,59 @@ def calculateA(input_,label,C,K):
         G = cvxopt.matrix(numpy.vstack(temp1,temp2))
         temp1 = numpy.zeros(num)
         temp2 = numpy.ones(num)*self.C
-        h = cvxopt.matrix(numpy.hstack(temp1,temp2))
+        h = cvxopt.matrix(numpy.hstack(temp1,temp2))        #P\q\A\b\G\h are parameters of cvxopt.solvers.qp() function
 
-    solution = cvxopt.solvers.qp(P,q,G,h,A,b)
+    solution = cvxopt.solvers.qp(P,q,G,h,A,b)               #figure out the model
     
-    a = numpy.ravel(solution['x'])
+    a = numpy.ravel(solution['x'])      #transfer the 'a' into a vector
     return a
 
-def calculateB(a,supportVectorLabel,supportVector,K,indexis):
+def calculateB(a,supportVectorLabel,supportVector,K,indexis):    #calculate the parameter 'b'
     b = 0
     for i in range(len(a)):
         b += supportVectorLabel[i]
         b -= numpy.sum(a*supportVectorLabel*K[indexis[i],supportVector])
-    b /= len(a)
+    b /= len(a)                         #set the  'b' with the mean value  
     return b
 
-def calculateWeight(kernel,features,a,supportVector,supportVectorLabel):
+def calculateWeight(kernel,features,a,supportVector,supportVectorLabel):     #calculate the model's weights according to a
     if kernel == linearKernel:
         w = numpy.zeros(features)
         for i in range(len(a)):
-            w += a[i]*supportVectorLabel[i]*supportVector[i]
+            w += a[i]*supportVectorLabel[i]*supportVector[i]     #linear kernel calculate as the linear model
     else:
-        w = None
+        w = None     #nonlinear nodel need not calculate ,we will process it later
     return w
 
-class SVM:
-    def __init__(self,kernel = linearKernel,C = None):
-        self.kernel = kernel
-        self.C = C
-        self.a = None
-        self.b = 0
-        self.w = []
-        self.supportVector = []
-        self.supportVectorLabel = []
+class SVM:           #SVM class
+    def __init__(self,kernel = linearKernel,C = None):    #init
+        self.kernel = kernel            #the kernel's algrithm
+        self.C = C                      #the soft margin's parameter
+        self.a = None                   #the process parameter
+        self.b = 0                      #the model's risdual
+        self.w = []                     #the model's weights
+        self.supportVector = []         #the support vector
+        self.supportVectorLabel = []    #the support's label
         if self.C is not None:
             self.C = float(self.C)
             
-    def fit(self,input_,label):
+    def fit(self,input_,label):         #fit data
         samples,features = input_.shape
         
-        K = makeKernelMatrix(input_,self.kernel)
-        a = calculateA(input_,label,self.C,K)
-        supportVector = a > 1e-5
-        indexis = numpy.arange(len(a))[supportVector]
-        self.a = a[supportVector]
-        self.supportVector = input_[supportVector]
-        self.supportVectorLabel = label[supportVector]
+        K = makeKernelMatrix(input_,self.kernel)          #calculate the kernel matrix
+        a = calculateA(input_,label,self.C,K)             #calculate the process parameter 'a'
+        supportVector = a > 1e-5                          #if a < 1e-5,then think a is 1
+        indexis = numpy.arange(len(a))[supportVector]     #the support vectors' indexis
+        self.a = a[supportVector]                         #the support vectors' a
+        self.supportVector = input_[supportVector]        #the support vectors
+        self.supportVectorLabel = label[supportVector]    #the support vectorss' label
         
         print(len(self.a),' support vectors out of ',samples,' points')
         
-        self.b = calculateB(self.a,self.supportVectorLabel,supportVector,K,indexis)
-        self.w = calculateWeight(self.kernel,features,self.a,self.supportVector,self.supportVectorLabel)
+        self.b = calculateB(self.a,self.supportVectorLabel,supportVector,K,indexis)   #calculate the model's risdual
+        self.w = calculateWeight(self.kernel,features,self.a,self.supportVector,self.supportVectorLabel)    #calculate the model's weights
         
-    def predict(self,input_):
+    def predict(self,input_):            #predict function
         if self.w is not None:
             return numpy.dot(inpt_,self.w) + self.b
         else:
@@ -109,7 +109,7 @@ class SVM:
                 predictLabel[i] = s
             return numpy.sign(predictLabel+self.b)
         
-def gen_non_lin_separable_data():
+def gen_non_lin_separable_data():        #creat the data
     mean1 = [-1, 2]
     mean2 = [1, -1]
     mean3 = [4, -4]
@@ -123,8 +123,8 @@ def gen_non_lin_separable_data():
     y2 = np.ones(len(X2)) * -1
     return X1, y1, X2, y2 
      
-def split_train(X1, y1, X2, y2):
-     X1_train = X1[:90]
+def split_train(X1, y1, X2, y2):        #split the train data from the original data
+     X1_train = X1[:90] 
      y1_train = y1[:90]
      X2_train = X2[:90]
      y2_train = y2[:90]
@@ -132,7 +132,7 @@ def split_train(X1, y1, X2, y2):
      y_train = np.hstack((y1_train, y2_train))
      return X_train, y_train
         
-def split_test(X1, y1, X2, y2):
+def split_test(X1, y1, X2, y2):         #split the test data from the original data
      X1_test = X1[90:]
      y1_test = y1[90:]
      X2_test = X2[90:]
@@ -141,7 +141,7 @@ def split_test(X1, y1, X2, y2):
      y_test = np.hstack((y1_test, y2_test))
      return X_test, y_test     
         
-def test_non_linear():
+def test_non_linear():                  #test
      X1, y1, X2, y2 = gen_non_lin_separable_data()
      X_train, y_train = split_train(X1, y1, X2, y2)
      print(y_train,'$$$')
